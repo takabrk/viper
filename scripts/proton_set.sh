1a3de777ace52d4d8e4178f0b583613e8192ca16
takabrk <takabrk@kvf.biglobe.ne.jp>
2020-11-27 12:42:36 +0900

    update20201127

---
+++ scripts/proton_set.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#proton_set.sh
+#Use winetricks and winecfg on Valve's Proton
+#Created by takamitsu hamada
+#2020/11/27
+
+while getopts e: OPT
+do
+  case $OPT in
+      e) e_n=$OPTARG
+         ;;
+  esac
+done
+case $e_n in
+    tricks)
+        WINEPREFIX=$HOME/.steam/steamapps/compatdata/$1/pfx winetricks
+    ;;
+    cfg)
+        WINEPREFIX=$HOME/.steam/steamapps/compatdata/$1/pfx winecfg
+    ;;
+    esac
---
+++ scripts/qemu.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo qemu-system-x86_64  -m 4048 -cdrom /home/ubuntu-builder/Ubuntu-18.04-amd64.iso
