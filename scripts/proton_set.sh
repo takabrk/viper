8cee7bb735058032defde802a908fa225dcdf5bc
takabrk <takabrk@kvf.biglobe.ne.jp>
2020-11-27 16:45:06 +0900

    update20201127

--- scripts/proton_set.sh
+++ scripts/proton_set.sh
@@ -5,6 +5,7 @@
 #website:https://vsrx.work
 #Updated 2020/11/27
 
+echo "$HOME/.steam/steamapps/compatdata/$0/pfx"
 while getopts e: OPT
 do
   case $OPT in
@@ -14,9 +15,15 @@ do
 done
 case $e_n in
     tricks)
-        WINEPREFIX=$HOME/.steam/steamapps/compatdata/$1/pfx winetricks
+        WINEPREFIX=$HOME/.steam/steamapps/compatdata/$3/pfx winetricks
     ;;
     cfg)
-        WINEPREFIX=$HOME/.steam/steamapps/compatdata/$1/pfx winecfg
+        WINEPREFIX=$HOME/.steam/steamapps/compatdata/$3/pfx winecfg
+    ;;
+    tricks32)
+        WINEARCH=win32 WINEPREFIX=$HOME/.steam/steamapps/compatdata/$3/pfx winetricks
+    ;;
+    cfg32)
+        WINEARCH=win32 WINEPREFIX=$HOME/.steam/steamapps/compatdata/$3/pfx winecfg
     ;;
     esac
