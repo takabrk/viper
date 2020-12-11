ab914b45008b982173d461e5b47bedca5ff80f9b
takabrk <takabrk@kvf.biglobe.ne.jp>
2020-12-12 04:33:33 +0900

    update20201212

--- build_noir_kernel/patches/build_noir_patch.sh
+++ build_noir_kernel/patches/build_noir_patch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 truncate noir.patch --size 0
-VERSIONPOINT="5.9.13"
+VERSIONPOINT="5.9.14"
 cat intel/clearlinux-v2-kai.patch \
        linux/patch-$VERSIONPOINT \
        custom_config.patch \
