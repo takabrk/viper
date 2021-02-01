eabf8c51b6151babbd785408110d50a94b166c94
takabrk <takabrk@kvf.biglobe.ne.jp>
2021-02-01 11:17:47 +0900

    update20210201

--- build_noir_kernel/510/build.sh
+++ build_noir_kernel/510/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #custom linux kernel build script
 #Created by takamitsu hamada
-#2021/1/28
+#2021/1/30
 
 while getopts e: OPT
 do
@@ -11,7 +11,7 @@ do
   esac
 done
 VERSIONBASE="5.10"
-VERSIONPOINT="5.10.11"
+VERSIONPOINT="5.10.12"
 case $e_num in
     base)
            wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$VERSIONBASE.tar.xz
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 Linux application "Viper Tools"
 Web site URL : http://vsrx.work
 Created by takamitsu hamada
-update:January 21,2020
+update:February 1,2020
 
 詳しいリファレンスは、以下で公開しています。
 
