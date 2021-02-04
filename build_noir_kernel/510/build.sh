50f01b0fb4a4c8c5c7f983245ae11f901382ddb9
takabrk <takabrk@kvf.biglobe.ne.jp>
2021-02-04 10:53:31 +0900

    update20210204

--- build_noir_kernel/510/build.sh
+++ build_noir_kernel/510/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #custom linux kernel build script
 #Created by takamitsu hamada
-#2021/1/30
+#2021/2/4
 
 while getopts e: OPT
 do
@@ -11,7 +11,7 @@ do
   esac
 done
 VERSIONBASE="5.10"
-VERSIONPOINT="5.10.12"
+VERSIONPOINT="5.10.13"
 case $e_num in
     base)
            wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$VERSIONBASE.tar.xz
--- build_noir_kernel/511/patches/build_noir_patch.sh
+++ build_noir_kernel/511/patches/build_noir_patch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSIONPOINT="5.11-rc5"
+VERSIONPOINT="5.11-rc6"
 NOIR_VERSION="noir"
 truncate noir.patch --size 0
 truncate custom_config.patch --size 0
