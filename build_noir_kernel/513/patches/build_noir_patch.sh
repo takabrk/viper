91df4e52a6d992ba835bf455926a43b31b7f64a0
takabrk <takabrk@kvf.biglobe.ne.jp>
2021-08-19 04:00:00 +0900

    update20210819

--- build_noir_kernel/513/patches/build_noir_patch.sh
+++ build_noir_kernel/513/patches/build_noir_patch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSIONPOINT="5.13.9"
+VERSIONPOINT="5.13.12"
 NOIR_VERSION="noir"
 truncate noir.patch --size 0
 truncate custom_config.patch --size 0
@@ -19,30 +19,17 @@ done
 case $e_num in
 muqss)
 cat add_noir_version.patch \
-       linux/patch-$VERSIONPOINT \
        custom_config.patch \
        add_noir_version.patch \
-       other/use_kyber2.patch \
-       other/0003-glitched-cfs.patch \
-       other/0003-sched-core-nr_migrate-256-increases-number-of-tasks-.patch \
+       other/0001-LL-kconfig-add-750Hz-timer-interrupt-kernel-config-o.patch \
        other/0004-mm-set-8-megabytes-for-address_space-level-file-read.patch \
+       other/use_kyber2.patch \
        aufs5/aufs5-base.patch \
        aufs5/aufs5-kbuild.patch\
        aufs5/aufs5-mmap.patch \
        aufs5/aufs5-standalone.patch \
-       other/0001-cpu-patches.patch \
-       other/ck1.patch \
-       other/0001-bbr2-patches.patch \
-       other/0001-futex-resync-from-gitlab.collabora.com.patch \
-       other/0001-futex2-resync-from-gitlab.collabora.com.patch \
        other/uksm-5.13.patch \
-       other/le9db-5.10.patch \
-       other/LRU.patch \
-       other/0001-ntfs3-patches.patch \
-       other/0001-zstd-patches.patch \
-       other/xanmod.patch \
-       other/0001-MultiQueue-Skiplist-Scheduler-v0.210.patch \
-       other/muqss_fixup.patch \
+       other/v5.13.12-lqx1.patch \
       > noir.patch
 ;;
 
