b614e2bcc527d5e21091ec03de9f896b78a124a0
takabrk <takabrk@kvf.biglobe.ne.jp>
2024-12-19 11:59:03 +0900

    update20241219

--- ramdisk/ramdisk_slider.py
+++ ramdisk/ramdisk_slider.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-#ramdisk_slider.py @takamitu hamada 20170107
-import os,os.path,sys
+#ramdisk_slider.py @takamitu hamada
+#December 19,2024
+
 import subprocess as sp
 try:
     import gi
@@ -16,63 +17,46 @@ class ramdisk_slider(object):
         self.tree = Gtk.Builder()
         self.tree.add_from_file(gladefile)
         dic = {
-            "on_button1_clicked" : self.on_button1_clicked,
-            "on_button2_clicked" : self.on_button2_clicked,
-            "on_button3_clicked" : self.on_button3_clicked
+            "on_button_rds_settings_clicked" : self.on_button_rds_settings_clicked,
+            "on_button_rds_reboot_clicked" : self.on_button_rds_reboot_clicked,
+            "on_button_rds_cancel_clicked" : self.on_button_rds_cancel_clicked
         }
         treeObj = self.tree.get_object
         self.tree.connect_signals(dic)
-        self.window = treeObj("button1")
-        self.window = treeObj("button2")
-        self.window = treeObj("button3")
-        self.combo1 = treeObj("comboboxtext1")
+        self.window = treeObj("button_rds_settings")
+        self.window = treeObj("button_rds_reboot")
+        self.window = treeObj("button_rds_cancel")
+        self.combo1 = treeObj("comboboxtext_rds")
         self.window = treeObj("window1")
         self.window.show_all()
         if(self.window):
             self.window.connect("destroy",Gtk.main_quit)
-#strage button
-    def on_button1_clicked(self,widget):
+#reboot button
+    def on_button_rds_reboot_clicked(self,widget):
         sp.call("sudo reboot".strip().split(" "))
 #Cancel button
-    def on_button2_clicked(self,widget):
+    def on_button_rds_cancel_clicked(self,widget):
         Gtk.main_quit()
 #Settings Button
-    def on_button3_clicked(self,widget):
-        if(self.combo1.get_active_text() == "200MB"):
-             msg = "200M"
-        if(self.combo1.get_active_text() == "500MB"):
-            msg = "500M"
+    def on_button_rds_settings_clicked(self,widget):
+        if(self.combo1.get_active_text() == "256MB"):
+             msg = "256M"
+        if(self.combo1.get_active_text() == "512MB"):
+            msg = "512M"
         if(self.combo1.get_active_text() == "1GB"):
             msg = "1GB"
-        if(self.combo1.get_active_text() == "1.5GB"):
-            msg = "1.5GB"
         if(self.combo1.get_active_text() == "2GB"):
             msg = "2GB"
-        if(self.combo1.get_active_text()  == "3GB"):
-            msg = "3GB"
         if(self.combo1.get_active_text() == "4GB"):
             msg = "4GB"
         if(self.combo1.get_active_text() == "8GB"):
             msg = "8GB"
-        f0 = open("/etc/rc.local","w")
-        f0.write("""#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "exit 0" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-
+        with open("/etc/rc.local","w") as f0:
+            f0.write("""#!/bin/sh -e
 mount -t tmpfs -w -o size=%s tmpfs /tmp
 exit 0
             """ % (msg))
-        f0.close()
-        print("Finished")
+        print("Finished tmpfs settings")
 if __name__ == "__main__":
     ramdisk_slider()
     Gtk.main()
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 Linux application "Viper Tools"
 Web site URL : http://vsrx.work
 Created by takamitsu hamada
-Updated December 18,2024
+Updated December 19,2024
 
 詳しいリファレンスは、以下で公開しています。
 
